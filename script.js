7fda0879f86d51af0575a1089fddafefb7304a09
eidrianf <adriano.francioni@gmail.com>
2023-04-19 23:39:34 -0300

    Formulario registro e inicio de sesion

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+///////////////////////////////////// PRIMERA PRE ENTREGA ///////////////////////////////////////////
+
+
 /*
 let huespedes;
 let nombresHuespedes = "";
@@ -41,6 +44,11 @@ if (huespedes != "") {
 };
 */
 
+
+///////////////////////////////////// SEGUNDA PRE ENTREGA ///////////////////////////////////////////
+
+
+/*
 
 class Hotel {
     constructor(id, nombre, ciudad, tarifaNoche){
@@ -134,7 +142,7 @@ if (tarifasFiltradas > 0) {
 
     if (respuesta) {
         do {
-            eleccion = prompt("Ingrese el nombre del alojamiento en el que desear reservar:" + "\n" + hotelesPresupuesto);
+            eleccion = prompt("Ingrese el nombre del alojamiento en el que desear reservar:" + "\n" + hotelesPresupuesto); //si ingresas mal el nombre se rompe.
         } while ((eleccion == "") || (eleccion == null)); 
         
         const eleccionReserva = hoteles.find((hotel) => hotel.nombre == eleccion);
@@ -155,5 +163,172 @@ if (tarifasFiltradas > 0) {
 } else{
     alert("No se encontraron alojamientos para su presupuesto por noche.")
 };
-   
 
+*/
+
+///////////////////////////////////// TERCERA PRE ENTREGA (PRUEBA) ///////////////////////////////////////////
+
+/*
+class Hotel {
+    constructor(id, nombre, ciudad, tarifaNoche){
+        this.id = id;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.tarifaNoche = tarifaNoche;
+    }
+};
+
+const hoteles = [];
+
+hoteles.push(new Hotel(01, "Melia", "Buenos Aires", 15000));
+hoteles.push(new Hotel(02, "NH Florida", "Buenos Aires", 23000));
+hoteles.push(new Hotel(03, "Faena Buenos Aires", "Buenos Aires", 38000));
+
+hoteles.push(new Hotel(04, "531 Hostel", "Córdoba", 8000));
+hoteles.push(new Hotel(05, "Amerian Executive Córdoba Hotel", "Córdoba", 18000));
+hoteles.push(new Hotel(06, "Azur Real Hotel Boutique", "Córdoba", 27000));
+
+hoteles.push(new Hotel(07, "Hotel Tres Reyes", "Bariloche", 12000));
+hoteles.push(new Hotel(08, "Peninsula Petit Hotel", "Bariloche", 24000));
+hoteles.push(new Hotel(09, "Llao LLao Hotel Resort", "Bariloche", 44000));
+
+hoteles.push(new Hotel(10, "Lujan de Cuyo B&B", "Mendoza", 10000));
+hoteles.push(new Hotel(11, "Posada El Encuentro", "Mendoza", 20000));
+hoteles.push(new Hotel(12, "Park Hyatt Mendoza", "Mendoza", 26000));
+
+hoteles.push(new Hotel(13, "Hotel Posada del Sol", "Salta", 14000));
+hoteles.push(new Hotel(14, "Sheraton Salta Hotel", "Salta", 22000));
+hoteles.push(new Hotel(15, "Hotel Solar de la Plaza", "Salta", 28000));
+
+hoteles.push(new Hotel(16, "Hosteria Valle Frio", "Ushuaia", 16000));
+hoteles.push(new Hotel(17, "Hotel Los Nires", "Ushuaia", 25000));
+hoteles.push(new Hotel(18, "Los Cauquenes Resort & Spa", "Ushuaia", 40000));
+
+
+let ciudadElegida = document.getElementById("ciudadElegida");
+
+ciudadElegida.addEventListener('change', (e) => {
+    let filtro = e.target.value;
+    let ciudadFiltrada;
+    if (filtro === "Buenos Aires") {
+        ciudadFiltrada = hoteles.filter((hotel) => hotel.ciudad === "Buenos Aires")
+    } else if (filtro === "Córdoba") {
+        ciudadFiltrada = hoteles.filter((hotel) => hotel.ciudad === "Córdoba")
+    } else if (filtro === "Bariloche") {
+        ciudadFiltrada = hoteles.filter((hotel) => hotel.ciudad === "Bariloche")
+    } else if (filtro === "Mendoza") {
+        ciudadFiltrada = hoteles.filter((hotel) => hotel.ciudad === "Mendoza")
+    } else if (filtro === "Salta") {
+        ciudadFiltrada = hoteles.filter((hotel) => hotel.ciudad === "Salta")
+    } else if (filtro === "Ushuaia") {
+        ciudadFiltrada = hoteles.filter((hotel) => hotel.ciudad === "Ushuaia")
+    };
+    console.log(ciudadFiltrada)
+});
+*/
+
+
+// const hotelesFiltrados = [];
+
+/*for (const hotel of filtroCiudad){
+    hotelesFiltrados.push("\n" + hotel.nombre);
+};
+
+alert("Se encontraron los siguientes hoteles en " + ciudadElegida + ":" + hotelesFiltrados);
+
+
+let presupuesto;
+
+function filtrarPresupuesto(ciudadElegida, presupuesto){
+    return ciudadElegida.filter((hotel) => hotel.tarifaNoche <= presupuesto);
+};
+
+do {
+    presupuesto = Number(prompt("Por favor ingrese cual es su presupuesto en pesos por noche:")) 
+} while ((presupuesto == "") || (presupuesto == null));
+
+let tarifasFiltradas = 0;
+
+for (const tarifa of filtroCiudad){
+    if (tarifa.tarifaNoche <= presupuesto){
+        tarifasFiltradas += 1;
+    };
+};
+
+if (tarifasFiltradas > 0) {
+    const filtroPresupuesto = filtrarPresupuesto(filtroCiudad, presupuesto);
+
+    const hotelesPresupuesto = [];
+
+    for (const hotel of filtroPresupuesto){
+        hotelesPresupuesto.push("\n" + hotel.nombre + " - Tarifa por noche: " + "$" + hotel.tarifaNoche);  
+    };
+
+    let respuesta = confirm("Se encontraron los siguientes hoteles según su presupuesto por noche: " + hotelesPresupuesto + "\n \nDesea reservar un alojamiento?");
+
+    let eleccion;
+
+    if (respuesta) {
+        do {
+            eleccion = prompt("Ingrese el nombre del alojamiento en el que desear reservar:" + "\n" + hotelesPresupuesto); //si ingresas mal el nombre se rompe.
+        } while ((eleccion == "") || (eleccion == null)); 
+        
+        const eleccionReserva = hoteles.find((hotel) => hotel.nombre == eleccion);
+
+        function calcularTotal (tarifaNoche, cantidadNoches) {
+            return tarifaNoche * cantidadNoches;
+        };
+
+        let cantidadNoches = Number(prompt("Por favor ingrese la cantidad de noches que desea hospedarse:"));
+
+        let total = calcularTotal(eleccionReserva.tarifaNoche, cantidadNoches);
+        console.log(total);
+
+        alert("Felicitaciones! Ha reservado en el alojamiento " + eleccionReserva.nombre + " por " + cantidadNoches + " noches, sumando un total de: " + "$" + total + ".");
+    } else {
+        alert("Muchas gracias por utilizar nuestro sistema de bùsqueda!");
+    };
+} else{
+    alert("No se encontraron alojamientos para su presupuesto por noche.")
+};
+*/
+
+
+///////////////////////////////////// TERCERA PRE ENTREGA ///////////////////////////////////////////
+
+let registro = document.getElementById("formularioRegistro");
+
+function registroUsuario (e) {
+    e.preventDefault();
+    e.target.value;
+    let email = document.getElementById("emailRegistro").value;
+    let password = document.getElementById("passwordRegistro").value;
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    formularioRegistro.reset();
+}
+
+registro.addEventListener('submit',registroUsuario);
+
+
+let inicioSesión = document.getElementById("formularioInicio");
+
+function inicioSesiónUsuario (e) {
+    e.preventDefault();
+    e.target.value;
+    let email = document.getElementById("emailInicio").value;
+    let password = document.getElementById("passwordInicio").value;
+    let emailLS = localStorage.getItem("email");
+    let passwordLS = localStorage.getItem("password");
+    formularioInicio.reset();
+    
+    if ((email === emailLS) && (password === passwordLS)){
+        let mensaje = document.getElementById("mensajeInicioSesion");
+        mensaje.innerHTML = `Bienvenido ${email}! Usted ha iniciado sesión correctamente.`
+    } else {
+        let mensaje = document.getElementById("mensajeInicioSesion");
+        mensaje.innerHTML = `Email o contraseña incorrectos.`
+    }
+};
+
+inicioSesión.addEventListener('submit', inicioSesiónUsuario);
